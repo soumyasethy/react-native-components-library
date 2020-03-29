39ed2106fcd6be8c8c387a2e33a70fd824c2375d
Soumya Ranjan Sethy <sethy.soumyaranjan@gmail.com>
2020-03-29 14:40:24 +0530

    fixed camera result callback react-native setup refactoring

--- src/components/hooks/CameraCard.tsx
+++ src/components/hooks/CameraCard.tsx
@@ -14,11 +14,11 @@ export const CameraCard = props => {
     "https://user-images.githubusercontent.com/2351721/31314483-7611c488-ac0e-11e7-97d1-3cfc1c79610e.png"
   );
   useEffect(() => {
-    /*ImagePickerX.openPicker({
-      multiple: true
-    }).then(images => {
-      console.warn(images);
-    });*/
+    // ImagePickerX.openPicker({
+    //   multiple: true
+    // }).then(images => {
+    //   console.warn(images);
+    // });
     console.warn("Camera selected-->", props.selected);
     props.selected && setImageProps(props.selected);
   }, []);
